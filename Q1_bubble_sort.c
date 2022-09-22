2b97986d813c54b4d341d9288ca3471d7fc030a7
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-09-22 14:24:43 +0530

    Add files via upload

--- Q1_bubble_sort.c
+++ Q1_bubble_sort.c
@@ -1,29 +1,40 @@
 #include <stdio.h>
 
-void bubble_sort(int arr[], int n){
-    for(int i=0; i<n-1; i++){
-        for(int j=0; j<n-i-1; j++){
-            if(arr[j] > arr[j+1]){
-                arr[j] = arr[j] + arr[j+1];
-                arr[j+1] = arr[j] - arr[j+1];
-                arr[j] = arr[j] - arr[j+1];
-            }
-        }
-    }
-
-    for(int i=0; i<n; i++){
-        printf("%d ", arr[i]);
-    }
-}
+void bubble_sort(int arr[], int n);
 
 int main(){
-    int arr[100], n;
-    printf("Enter the size of the array : ");
-    scanf("%d", &n);
-    for(int i=0; i<n; i++){
-        printf("Enter value of element %d : ", i+1);
-        scanf("%d", &arr[i]);
-    }
-    printf("Sorted array: ");
-    bubble_sort(arr, n);
+	int arr[20], n;
+	printf("Enter the size of the array : ");
+	scanf("%d", &n);
+	for(int i=0; i<n; i++){
+		printf("Enter the value of arr[%d] : ", i);
+		scanf("%d", &arr[i]);
+	}
+	
+	printf("\nBefore Bubble Sort\n");
+	for(int i=0; i<n; i++){
+		printf("%d ", arr[i]);
+	}
+	
+	bubble_sort(arr, n);
+	
+	printf("\n\nAfter Bubble Sort\n");
+	for(int i=0; i<n; i++){
+		printf("%d ", arr[i]);
+	}
+	
+	printf("\n\n");
+	return 0;
+}
+
+void bubble_sort(int arr[], int n){
+	for(int i=0; i<n-1; i++){
+		for(int j=0; j<n-i-1; j++){
+			if(arr[j] > arr[j+1]){
+				int temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+	}
 }
--- Q1_selection_sort.c
+++ Q1_selection_sort.c
@@ -1,33 +1,45 @@
 #include <stdio.h>
 
-void selection_sort(int arr[], int n){
-    int min_index;
-    for(int i=0; i<n; i++){
-        min_index = i;
-        for(int j=i+1; j<n; j++){
-            if(arr[j] < arr[min_index])
-                min_index = j;
-        }
-        if(min_index != i){
-            arr[i] = arr[i] + arr[min_index];
-            arr[min_index] = arr[i] - arr[min_index];
-            arr[i] = arr[i] - arr[min_index];
-        }
-    }
-
-    for(int i=0; i<n; i++){
-        printf("%d ", arr[i]);
-    }
-}
+void selection_sort(int arr[], int n);
 
 int main(){
-    int arr[100], n;
-    printf("Enter the size of the array : ");
-    scanf("%d", &n);
-    for(int i=0; i<n; i++){
-        printf("Enter value of element %d : ", i+1);
-        scanf("%d", &arr[i]);
-    }
-    printf("Sorted array: ");
-    selection_sort(arr, n);
+	int arr[20], n;
+	printf("Enter the size of the array : ");
+	scanf("%d", &n);
+	for(int i=0; i<n; i++){
+		printf("Enter the value of arr[%d] : ", i);
+		scanf("%d", &arr[i]);
+	}
+	
+	printf("\nBefore Selection Sort\n");
+	for(int i=0; i<n; i++){
+		printf("%d ", arr[i]);
+	}
+	
+	selection_sort(arr, n);
+	
+	printf("\n\nAfter Selection Sort\n");
+	for(int i=0; i<n; i++){
+		printf("%d ", arr[i]);
+	}
+	printf("\n\n");
+	
+	return 0;
+	
+}
+
+void selection_sort(int arr[], int n){
+	int min_index;
+	for(int i=0; i<n; i++){
+		min_index = i;
+		for(int j=i+1; j<n; j++){
+			if(arr[j] < arr[min_index])
+				min_index = j;
+		}
+		if(min_index != i){
+			int temp = arr[i];
+			arr[i] = arr[min_index];
+			arr[min_index] = temp;
+		}
+	}
 }
