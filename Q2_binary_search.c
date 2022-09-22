70fb4c24bb67e1308c3b23185d34d2aafb2e4f30
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-09-22 14:25:05 +0530

    Add files via upload

--- Q2_binary_search.c
+++ Q2_binary_search.c
@@ -1,49 +1,45 @@
 #include <stdio.h>
 
-int binary_search_iterative(int array[], int n, int x){
-    int low=0, high=n-1, mid=0;
-    while(low <= high){
-        mid = (high + low) / 2;
-        if(array[mid] < x)
-            low = mid + 1;
-        else if(array[mid] > x)
-            high = mid - 1;
-        else
-            return mid;
-    }
-    return -1;
-}
-
-int binary_search_recursive(int array[], int low, int high, int x){
-    int mid = 0;
-    if(high >= low){
-        mid = (high + low) / 2;
-        if(array[mid] == x)
-            return mid;
-        else if(array[mid] > x)
-            return binary_search_recursive(array, low, mid-1, x);
-        else
-            return binary_search_recursive(array, mid+1, high, x);
-    } else{
-        return -1;
-    }
+int binary_search(int arr[], int n, int x){
+	int low=0, high=n-1, mid=0;
+	while(low <= high){
+		mid = (low + high) / 2;
+		if(arr[mid] < x)
+			low = mid + 1;
+		else if(arr[mid] > x)
+			high = mid - 1;
+		else
+			return mid;
+	}
+	return -1;
 }
 
 int main(){
-    int arr[100], n, value;
-    printf("Enter the number of elements in the sorted array : ");
-    scanf("%d", &n);
-
-    for(int i=0; i<n; i++){
-        printf("Enter the values of element %d : ", i+1);
-        scanf("%d", &arr[i]);
-    }
-
-    printf("Enter the value to search for : ");
-    scanf("%d", &value);
-
-    printf("Output form iterative binary search : %d\n", binary_search_iterative(arr, n, value));
-    printf("Output form recursive binary search : %d", binary_search_recursive(arr, 0, n-1, value));
-
-    return 0;
+	int arr[20], n, x, found;
+	
+	printf("Enter the size of the sorted array : ");
+	scanf("%d", &n);
+	
+	for(int i=0; i<n; i++){
+		printf("Enter the value of arr[%d] : ", i);
+		scanf("%d", &arr[i]);
+	}
+	
+	printf("\nEnter the element to search from the sorted array : ");
+	scanf("%d", &x);
+	
+	printf("\nThe sorted array is \n");
+	for(int i=0; i<n; i++){
+		printf("%d ", arr[i]);
+	}
+	
+	found = binary_search(arr, n, x);
+	
+	if(found != -1)
+		printf("\n\n%d is found at index %d of the sorted array", x, found);
+	else
+		printf("\n\n%d is not found in the sorted array", x);
+	printf("\n\n");
+	return 0;
+	
 }
--- Q2_linear_search.c
+++ Q2_linear_search.c
@@ -1,32 +1,41 @@
 #include <stdio.h>
 
-int main(){
-    int array[100], n, value, index, found=0;
-
-    printf("Enter the number of elements in the array : ");
-    scanf("%d", &n);
-    
-    for(int i=0; i<n; i++){
-        printf("Enter the value of element %d : ", i+1);
-        scanf("%d", &array[i]);
-    }
-
-    printf("Enter the value to search for : ");
-    scanf("%d", &value);
-
-    for(int i=0; i<n; i++){
-        if(array[i] == value){
-            index = i+1;
-            found = 1;
-            break;
-        }
-    }
-
-    if(found){
-        printf("Value found in the array at index %d", index);
-    } else{
-        printf("Value not found");
-    }
-
-    return 0;
+int linear_search(int arr[], int n, int x){
+	for(int i=0; i<n; i++){
+		if(arr[i] == x)
+			return i;
+	}
+	return -1;
 }
+
+int main(){
+	int arr[20], n, x, found;
+	
+	printf("Enter the size of the array : ");
+	scanf("%d", &n);
+	
+	for(int i=0; i<n; i++){
+		printf("Enter the value of arr[%d] : ", i);
+		scanf("%d", &arr[i]);
+	}
+	
+	printf("\nEnter the element to search for : ");
+	scanf("%d", &x);
+	
+	printf("\nThe array is \n");
+	for(int i=0; i<n; i++){
+		printf("%d ", arr[i]);
+	}
+	
+	found = linear_search(arr, n, x);
+	
+	if(found != -1)
+		printf("\n\n%d is found at index %d of the array", x, found);
+	else
+		printf("\n\n%d is not found in the array", x);
+	printf("\n\n");
+	return 0;
+	
+}
+
+
