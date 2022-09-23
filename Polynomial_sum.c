10f7b907520d82f0115e2808c6bcf50291326d95
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-09-23 18:22:27 +0530

    Add files via upload

--- Polynomial_sum.c
+++ Polynomial_sum.c
@@ -1,52 +1,123 @@
-#include <stdio.h>
+#include<stdio.h>
 
-struct Polynomial {
-    float coeff;
-    int expo;
+struct poly {
+	 int coeff;
+	 int expo;
 };
 
+struct poly p1[10], p2[10], p3[10];
+
+int readPoly(struct poly []);
+int addPoly(struct poly [], struct poly [], int, int, struct poly []);
+void displayPoly(struct poly [], int terms);
+
 int main(){
-    struct Polynomial X1[3] = {{7, 4}, {5, 2}, {3, 1}};
-    struct Polynomial X2[3] = {{5, 3}, {3, 1}, {-8, 0}};
-    struct Polynomial X3[5];
-    int i=0, j=0, k=0;
-    while(i<3 && j<3){
-        if(X1[i].expo == X2[j].expo){
-            X3[k].expo = X1[i].expo;
-            X3[k].coeff = X1[i].coeff + X2[j].coeff;
-            i++;
-            j++;
-            k++;
-        } else if(X1[i].expo > X2[j].expo){
-            X3[k].expo = X1[i].expo;
-            X3[k].coeff = X1[i].coeff;
-            i++;
-            k++;
-        } else if(X1[i].expo < X2[j].expo){
-            X3[k].expo = X2[j].expo;
-            X3[k].coeff = X2[j].coeff;
-            j++;
-            k++;
-        }
-    }
+    int t1, t2, t3;
 
-    while(j < 3){
-        X3[k].expo = X2[j].expo;
-        X3[k].coeff = X2[j].coeff;
-        j++;
-        k++;
-    }
+	t1 = readPoly(p1);
+	printf(" \n First polynomial : ");
+	displayPoly(p1, t1);
 
-    while(i < 3){
-        X3[k].expo = X1[i].expo;
-        X3[k].coeff = X1[i].coeff;
-        i++;
-        k++;
-    }
+	t2 = readPoly(p2);
+	printf(" \n Second polynomial : ");
+	displayPoly(p2, t2);
 
-    printf("%f %d\n", X3[0].coeff, X3[0].expo);
-    printf("%f %d\n", X3[1].coeff, X3[1].expo);
-    printf("%f %d\n", X3[2].coeff, X3[2].expo);
-    printf("%f %d\n", X3[3].coeff, X3[3].expo);
-    printf("%f %d\n", X3[4].coeff, X3[4].expo);
+	t3 = addPoly(p1, p2, t1, t2, p3);
+	printf(" \n\n Resultant polynomial after addition : ");
+	displayPoly(p3, t3);
+	printf("\n");
+
+	return 0;
 }
+
+int readPoly(struct poly p[10]){
+	int t1, i;
+
+	printf("\n\n Enter the total number of terms in the polynomial:");
+	scanf("%d", &t1);
+
+	printf("\n Enter the COEFFICIENT and EXPONENT in DESCENDING ORDER\n");
+	for(i=0; i<t1; i++){
+		printf("   Enter the Coefficient(%d): ", i+1);
+		scanf("%d", &p[i].coeff);
+		printf("      Enter the exponent(%d): ", i+1);
+		scanf("%d", &p[i].expo);        
+	}
+	return t1;
+}
+
+int addPoly(struct poly p1[10], struct poly p2[10], int t1, int t2, struct poly p3[10]){
+	int i=0, j=0, k=0;
+
+	while(i<t1 && j<t2){
+		if(p1[i].expo == p2[j].expo){
+			p3[k].coeff = p1[i].coeff + p2[j].coeff;
+			p3[k].expo = p1[i].expo;
+			i++;
+			j++;
+			k++;
+		} else if(p1[i].expo > p2[j].expo){
+			p3[k].coeff = p1[i].coeff;
+			p3[k].expo = p1[i].expo;
+			i++;
+			k++;
+		} else {
+			p3[k].coeff = p2[j].coeff;
+			p3[k].expo = p2[j].expo;
+			j++;
+			k++;
+		}
+	}
+
+	while(i<t1){
+		p3[k].coeff = p1[i].coeff;
+		p3[k].expo = p1[i].expo;
+		i++;
+		k++;
+	}
+	
+	while(j<t2){
+		p3[k].coeff = p2[j].coeff;
+		p3[k].expo = p2[j].expo;
+		j++;
+		k++;
+	}
+
+	return k;
+}
+
+void displayPoly(struct poly p[10], int term){
+	for(int k=0;k<term-1;k++)
+	printf("%d(x^%d)+",p[k].coeff,p[k].expo);
+	printf("%d(x^%d)",p[term-1].coeff,p[term-1].expo);
+}
+
+/*
+
+
+ Enter the total number of terms in the polynomial:3
+
+ Enter the COEFFICIENT and EXPONENT in DESCENDING ORDER
+   Enter the Coefficient(1): 3
+      Enter the exponent(1): 2
+   Enter the Coefficient(2): 2
+      Enter the exponent(2): 1
+   Enter the Coefficient(3): 1
+      Enter the exponent(3): 0
+ 
+ First polynomial : 3(x^2)+2(x^1)+1(x^0)
+
+ Enter the total number of terms in the polynomial:3
+
+ Enter the COEFFICIENT and EXPONENT in DESCENDING ORDER
+   Enter the Coefficient(1): 4
+      Enter the exponent(1): 4
+   Enter the Coefficient(2): 3
+      Enter the exponent(2): 1
+   Enter the Coefficient(3): 1
+      Enter the exponent(3): 0
+ 
+ Second polynomial : 4(x^4)+3(x^1)+1(x^0) 
+
+ Resultant polynomial after addition : 4(x^4)+3(x^2)+5(x^1)+2(x^0)
+ */
