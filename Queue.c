0a25308afffc3bef8421820233bb55c21f4fd84b
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2022-09-27 07:44:03 +0530

    Update Queue.c

--- Queue.c
+++ Queue.c
@@ -1,47 +1,63 @@
-#include<stdio.h>
-#define n 5
-int main()
-{
-    int queue[n],ch=1,front=0,rear=0,i,j=1,x=n;
-    printf("Queue using Array");
-    printf("\n1.Insertion \n2.Deletion \n3.Display \n4.Exit\n");
-    do {
-        printf("\nEnter the Choice : ");
-        scanf("%d",&ch);
-        switch(ch){
-            case 1:
-                if(rear==x)
-                    printf("Queue is Full\n");
-                else {
-                    printf("Enter no %d : ",j++);
-                    scanf("%d",&queue[rear++]);
-                }
-                break;
-            case 2:
-                if(front==rear){
-                    printf("Queue is empty\n");
-                } else {
-                    printf("Deleted Element is %d\n",queue[front++]);
-                    x++;
-                }
-                break;
-            case 3:
-                if(front==rear)
-                    printf("Queue is Empty\n");
-                else {
-                    printf("Queue Elements are\n");
-                    for(i=front; i<rear; i++){
-                        printf("%d ",queue[i]);
-                        printf("\n");
-                    }
-                }
-                break;
-            case 4:
-                break;
-            default:
-                printf("Wrong Choice: please see the options\n");
-        }
-    } while(ch != 4);
-    return 0;
+#include <stdio.h>
+
+int queue[100], n, rear=-1, front=-1, option;
+
+void Enqueue();
+void Dequeue();
+void Display();
+
+int main(){
+	printf("Enter the size of the queue : ");
+	scanf("%d", &n);
+
+	printf("\n1) Enqueue\n2) Dequeue\n3) Display\n4) Exit\n");
+	do {
+		printf("\nSelect your option : ");
+		scanf("%d", &option);
+
+		switch(option){
+			case 1:
+				Enqueue();
+				break;
+			case 2:
+				Dequeue();
+				break;
+			case 3:
+				Display();
+				break;
+			case 4:
+				break;
+			default:
+				printf("Please select valid option\n");
+		}
+	} while(option != 4);
 }
 
+void Enqueue(){
+	if(rear >= n-1){
+		printf("Queue is in OVER FLOW condition\n");
+	} else {
+		if(front == -1)
+			front = 0;
+		printf("Enter the value to enqueue : ");
+		scanf("%d", &queue[++rear]);
+	}
+}
+
+void Dequeue(){
+	if(front == -1 || front > rear)
+		printf("Queue is in UNDER FLOW condition\n");
+	else
+		printf("%d is deleted\n", queue[front++]);
+}
+
+void Display(){
+	if(front == -1 || front > rear)
+		printf("Queue is empty\n");
+	else {
+		printf("Queue element are\n");
+		for(int i=front; i<=rear; i++){
+			printf("%d\n", queue[i]);
+		}
+	}
+}
